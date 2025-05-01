b7fccd7a4d1ddbaca1a89f18859c98b57a025fc9
Currency4time <dbarontf@gmail.com>
2025-05-01 10:18:00 -0600

    fixed a broken function

--- mission/mission.js
+++ mission/mission.js
@@ -1,14 +1,15 @@
 const themeSelector = document.querySelector("#brightness")
 function changeTheme() {
-    const logo = document.querySelector('#brightness')
+    const logo = document.querySelector('#logo');
+    const body = document.body;
+
+    if (themeSelector.value==="dark"){
+        body.classList.add('dark')
+        logo.src="byui-logo_white.png"
+    } else{
+        body.classList.remove('dark')
+        logo.src="byui-logo_blue.webp"
+    }
 }
 
-if (themeSelector.value==="dark"){
-    document.body.classList.add('dark')
-    logo.src="byui-logo_white.png"
-} else{
-    body.classList.remove('dark')
-    logo.src="byui-logo_blue.webp"
-}
-
-themseSelector.addEventListener('change',changeTheme)
+themeSelector.addEventListener('change',changeTheme)
